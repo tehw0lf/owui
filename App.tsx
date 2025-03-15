2cb02dab351716a54d007576852538a92b4c15fa
tehw0lf <tehwolf@protonmail.com>
2025-03-15 02:52:42 +0100

    add timeout

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import {WebView} from 'react-native-webview';
 
 const App = () => {
   const injectedJavaScript = `
+  setTimeout(() => {
     document.getElementsByName('email')[0].value = "lars@local.local";
     document.getElementsByName('current-password')[0].value = "oL9PCJ;&,$w=s>+\\srD[ W37tc$3X<zKZ?M4/2MC3o;|d7mg>-i%K%X?j(e'=5Df";
     document.getElementsByTagName('form')[0].submit();
+    }, 500)
   `;
-
   return (
     <WebView
       source={{uri: 'http:192.168.42.20:3000/auth'}}
